bee215bf4990d41a6740ab68e03b2ba9b1701d9d
Davi Lima <davilima6@gmail.com>
2017-10-21 15:16:04 +0200

    WiP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { RESTAPIModule } from '@plone/restapi-angular';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
   MatListModule,
-  MatIconModule
+  MatIconModule,
+  MatFormFieldModule,
+  MatPlaceholder,
+  MatButtonModule
 } from '@angular/material';
 
 import { environment } from '../environments/environment';
@@ -34,7 +37,10 @@ import { PastanagaToolbarComponent } from './pastanaga-toolbar/pastanaga-toolbar
     BrowserAnimationsModule,
     MatListModule,
     MatIconModule,
-    IconsModule
+    IconsModule,
+    MatFormFieldModule,
+    MatPlaceholder,
+    MatButtonModule
   ],
   providers: [
     {
--- src/app/pastanaga-login/pastanaga-login.component.ts
+++ src/app/pastanaga-login/pastanaga-login.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+// import {MatMenuModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+
+// import {Component} from '@angular/core';
+
+// /**
+//  * @title Button varieties
+//  */
+// @Component({
+//   selector: 'button-types-example',
+//   templateUrl: 'button-types-example.html',
+//   styleUrls: ['button-types-example.css'],
+// })
+// export class ButtonTypesExample {}
+
 
 @Component({
   selector: 'app-pastanaga-login',
